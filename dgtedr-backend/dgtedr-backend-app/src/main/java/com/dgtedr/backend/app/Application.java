92247957f58c8fbb56504e565db6171280b3b7e4
mark.martinez <mbmartinez@exist.com>
2018-06-06 14:26:15 +0800

    Initial commits update

---
+++ dgtedr-backend/dgtedr-backend-app/src/main/java/com/dgtedr/backend/app/Application.java
@@ -0,0 +1,10 @@
+package com.dgtedr.backend.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {
+    "com.efs.core"
+})
+public class Application {
+
+}
