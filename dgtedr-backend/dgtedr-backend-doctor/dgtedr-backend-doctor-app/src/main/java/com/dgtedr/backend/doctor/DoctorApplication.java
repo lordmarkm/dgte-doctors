3bf9404a7448a898d74c4f9763f984153d7737f5
mark.martinez <mbmartinez@exist.com>
2018-06-11 14:01:04 +0800

    Fix the errors from upping spring boot parent version

--- dgtedr-backend/dgtedr-backend-doctor/dgtedr-backend-doctor-app/src/main/java/com/dgtedr/backend/doctor/DoctorApplication.java
+++ dgtedr-backend/dgtedr-backend-doctor/dgtedr-backend-doctor-app/src/main/java/com/dgtedr/backend/doctor/DoctorApplication.java
@@ -2,11 +2,13 @@ package com.dgtedr.backend.doctor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.dgtedr.backend.doctor.service.SpecialtyService;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableJpaRepositories(basePackageClasses = SpecialtyService.class,
     repositoryImplementationPostfix = "CustomImpl")
 public class DoctorApplication {
