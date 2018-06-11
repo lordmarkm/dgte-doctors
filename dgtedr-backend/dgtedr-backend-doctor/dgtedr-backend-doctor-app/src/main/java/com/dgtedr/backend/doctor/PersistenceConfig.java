d2a0d4b2efdbe1fc273989d0764df14d62afd5d9
mark.martinez <mbmartinez@exist.com>
2018-06-11 15:38:40 +0800

    Persistence isn't working

--- dgtedr-backend/dgtedr-backend-doctor/dgtedr-backend-doctor-app/src/main/java/com/dgtedr/backend/doctor/PersistenceConfig.java
+++ dgtedr-backend/dgtedr-backend-doctor/dgtedr-backend-doctor-app/src/main/java/com/dgtedr/backend/doctor/PersistenceConfig.java
@@ -5,8 +5,8 @@ import java.beans.PropertyVetoException;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
