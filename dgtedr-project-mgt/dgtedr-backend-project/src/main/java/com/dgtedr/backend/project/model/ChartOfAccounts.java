502a1a8523f69287e2b0b753b314a4e457d797b4
Mark Martinez <mbmartinez@exist.com>
2018-07-19 20:28:04 +0800

    adding additional entities to project-project

---
+++ dgtedr-project-mgt/dgtedr-backend-project/src/main/java/com/dgtedr/backend/project/model/ChartOfAccounts.java
@@ -0,0 +1,10 @@
+package com.dgtedr.backend.project.model;
+
+import javax.persistence.Entity;
+
+import com.efs.core.jpa.model.BaseEntity;
+
+@Entity(name = "chart_of_accounts")
+public class ChartOfAccounts extends BaseEntity {
+
+}
---
+++ dgtedr-project-mgt/dgtedr-backend-project/src/main/java/com/dgtedr/backend/project/model/Funding.java
@@ -0,0 +1,12 @@
+package com.dgtedr.backend.project.model;
+
+import javax.persistence.Entity;
+
+import com.efs.core.jpa.model.BaseEntity;
+
+@Entity(name = "funding")
+public class Funding extends BaseEntity {
+
+    private static final long serialVersionUID = 1L;
+
+}
