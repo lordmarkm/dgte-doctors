8feb24fac17608aa2099644457da953601f98644
Mark Martinez <mbmartinez@exist.com>
2018-08-11 21:14:09 +0800

    working fund source & project resources

--- dgtedr-project-mgt/dgtedr-project-shared/dgtedr-project-shared-dto/src/main/java/com/dgtedr/project/shared/dto/BaseInfo.java
+++ dgtedr-project-mgt/dgtedr-project-shared/dgtedr-project-shared-dto/src/main/java/com/dgtedr/project/shared/dto/BaseInfo.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  *
  * @author mbmartinez, Sep 27, 2017
@@ -17,6 +19,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 })
 public abstract class BaseInfo extends com.efs.core.dto.BaseInfo {
 
+    @ApiModelProperty(allowableValues = "project, fund_source, fund_contribution")
     public abstract String getType();
 
 }
--- dgtedr-project-mgt/dgtedr-project-shared/dgtedr-project-shared-dto/src/main/java/com/dgtedr/project/shared/dto/FundSourceInfo.java
+++ dgtedr-project-mgt/dgtedr-project-shared/dgtedr-project-shared-dto/src/main/java/com/dgtedr/project/shared/dto/FundSourceInfo.java
@@ -6,15 +6,26 @@ public class FundSourceInfo extends BaseInfo {
 
     public static final String DTO_TYPE = "fund_source";
 
+    private String name;
+
     @Override
     public ToStringCreator toStringCreator() {
         return super.toStringCreator()
+                .append("name", name)
                 .append("type", DTO_TYPE);
     }
 
     @Override
     public String getType() {
         return DTO_TYPE;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
 }
