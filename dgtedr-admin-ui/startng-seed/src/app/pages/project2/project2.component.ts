11cfe63f803d44413fc97750957ad6b3833967be
mark.martinez <mbmartinez@exist.com>
2018-08-28 07:56:18 +0800

    Expand startng-seed

---
+++ dgtedr-admin-ui/startng-seed/src/app/pages/project2/project2.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-project2',
+  templateUrl: './project2.component.html',
+  styleUrls: ['./project2.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class Project2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
