76d5a91b4380ac9cd68b79f290599c3bfb27ed7a
mark.martinez <mbmartinez@exist.com>
2018-09-26 20:34:36 +0800

    what the fuck

--- ampota/ampota-gateway/src/main/java/com/ampota/gateway/resource/BundleGatewayResource.java
+++ ampota/ampota-gateway/src/main/java/com/ampota/gateway/resource/BundleGatewayResource.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,11 @@ public class BundleGatewayResource {
             @RequestParam(required = false) @ApiParam(value = "The search term, example: name==test;deleted==false") String term,
             Pageable page) {
         return client.rqlSearch(term, page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BundleInfo> findOne(@PathVariable long id) {
+        return client.findOne(id);
     }
 
     @PostMapping
--- ampota/ampota-shared/ampota-shared-client/src/main/java/com/ampota/shared/client/BundleClient.java
+++ ampota/ampota-shared/ampota-shared-client/src/main/java/com/ampota/shared/client/BundleClient.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,9 @@ public interface BundleClient {
      */
     @GetMapping("/api/bundle")
     ResponseEntity<Page<BundleInfo>> rqlSearch(@RequestParam String term, @RequestBody Pageable page);
+
+    @GetMapping("/api/bundle/{id}")
+    ResponseEntity<BundleInfo> findOne(@PathVariable long id);
 
     @PostMapping("/api/bundle")
     ResponseEntity<BundleInfo> save(@RequestBody BundleInfo bundle);
