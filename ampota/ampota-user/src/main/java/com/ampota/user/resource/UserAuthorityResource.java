948bc6d7c73e7608298ed044324e0e55310d1f98
mark.martinez <mbmartinez@exist.com>
2018-10-04 15:58:17 +0800

    renamed due to user authority refactor

--- ampota/ampota-user/src/main/java/com/ampota/user/resource/UserAuthorityResource.java
+++ ampota/ampota-user/src/main/java/com/ampota/user/resource/UserAuthorityResource.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.collect.Lists;
 
-import xyz.quadx.xpay.shared.firebase.client.UserAuthorityClient;
+import xyz.quadx.xpay.shared.firebase.user.client.UserAuthorityClient;
 
 @RestController
 @RequestMapping("/api/user-authority")
